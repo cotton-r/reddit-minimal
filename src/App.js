5c8a9d653b90cd1f1eeca542376f869aa93e72fa
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-03 23:35:45 +0100

    create App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import './normalize.css';
+
+import React, { useEffect } from 'react';
+
+import SearchBar from './components/SearchBar/SearchBar';
+import PostsContainer from './components/PostsContainer/PostsContainer';
+import Subreddits from './components/Subreddits/Subreddits';
+
+import redditlogo from './redditlogo.png';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { getSubredditPosts } from './app/Reddit';
+import { changePosts } from './components/PostsContainer/PostsContainerSlice';
+
+
 
 function App() {
+
+  const activeSub = useSelector(state => state.subreddits.activeSubreddit);
+  const dispatch = useDispatch();
+
+  useEffect(() => getSubredditPosts(activeSub)
+  .then(response => {
+    dispatch(changePosts(response));
+  }), [activeSub]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className="header">
+        <h1 className="mainHeader">Reddit Minimal</h1>
+        <SearchBar className="searchBar"/>
       </header>
+
+      <Subreddits logo={redditlogo} />
+      <PostsContainer />
+
     </div>
-  );
+  )
 }
 
 export default App;
