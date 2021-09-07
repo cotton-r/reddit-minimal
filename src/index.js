c704b175d0951b2d8ff19d6cf11b64d5a57475fc
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-09-07 15:10:54 +0100

    fixed bug where 1 css file contained extra semi-colon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './normalize.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './normalize.css';
 import { Provider } from 'react-redux';
 import store from './app/store';
 
